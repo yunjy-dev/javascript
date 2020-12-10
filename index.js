9d4d9687adb80cd29928a3d26d4a3729c87ba030
yunjy-dev <yunjy.dev@gmail.com>
2020-12-10 22:17:44 +0900

    createElement() and append()

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert('initial');
+var body = document.body;
+var word = document.createElement('div');
+word.textContent = '시작';
+body.append(word);
+
+var input = document.createElement('input');
+body.append(input);
+
+var btn = document.createElement('button');
+body.append(btn);
+btn.textContent = '입력';
+
+body.append(document.createElement('br'));
+
+var result = document.createElement('result');
+body.append(result);
+result.textContent = '결과'
+
