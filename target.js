ba63437789912715ab72b939f60f79c7b1b0ad3d
yunjy-dev <yunjy.dev@gmail.com>
2020-12-10 23:16:12 +0900

    e.target but failed

--- target.js
+++ target.js
@@ -4,14 +4,27 @@ var table = document.createElement('table');
 var rows = [];
 var cols = [];
 
+var callback = function (e){
+    console.log(e.target);
+    console.log(e.target.parentNode);
+    console.log(e.target.parentNode.parentNode);
+
+    var thisRow = rows.indexOf(e.target.parentNode);
+    var thisCol = cols.indexOf(e.currentTarget);
+
+    console.log('thisRow', thisRow, 'thisCol', thisCol);
+    console.log('e.target', e.target);
+    console.log('e.target', cols.indexOf(e.currentTarget));
+
+}
+
 for(var i=1; i<=3; i++){
     var row = document.createElement('tr');
     rows.push(row);
     cols.push([]);
     for(var j=1; j<=3; j++){
         var col = document.createElement('td');
-        col.addEventListener('click', function callback(){
-        });
+        col.addEventListener('click', callback);
         cols[i-1].push(col);
         row.appendChild(col);
     }
